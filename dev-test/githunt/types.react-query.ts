a70964409e882ace42756e3f9f86a0eb8819a120
Evan Jones <evan.a.jones3@gmail.com>
2023-01-16 11:39:21 -0500

    revert PR #8497: "fixes issue #7549" (#25)
    * revert PR #8497 fixes issue #7549
    
    * generate examples
    
    * add changeset

--- dev-test/githunt/types.react-query.ts
+++ dev-test/githunt/types.react-query.ts
@@ -475,7 +475,7 @@ export const useInfiniteCommentQuery = <TData = CommentQuery, TError = unknown>(
     metaData =>
       fetcher<CommentQuery, CommentQueryVariables>(dataSource.endpoint, dataSource.fetchParams || {}, CommentDocument, {
         ...variables,
-        ...(metaData.pageParam ? { [pageParamKey]: metaData.pageParam } : {}),
+        ...(metaData.pageParam ?? {}),
       })(),
     options
   );
@@ -516,7 +516,7 @@ export const useInfiniteCurrentUserForProfileQuery = <TData = CurrentUserForProf
         dataSource.endpoint,
         dataSource.fetchParams || {},
         CurrentUserForProfileDocument,
-        { ...variables, ...(metaData.pageParam ? { [pageParamKey]: metaData.pageParam } : {}) }
+        { ...variables, ...(metaData.pageParam ?? {}) }
       )(),
     options
   );
@@ -552,7 +552,7 @@ export const useInfiniteFeedQuery = <TData = FeedQuery, TError = unknown>(
     metaData =>
       fetcher<FeedQuery, FeedQueryVariables>(dataSource.endpoint, dataSource.fetchParams || {}, FeedDocument, {
         ...variables,
-        ...(metaData.pageParam ? { [pageParamKey]: metaData.pageParam } : {}),
+        ...(metaData.pageParam ?? {}),
       })(),
     options
   );
