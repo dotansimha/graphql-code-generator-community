484836632ac339fa434eea573bfb32680157eae1
Arnout Boks <arnout.boks@moxio.com>
2023-09-18 16:10:56 +0200

    Add failing test as a reproduction case for duplicate imports with near-operation-file and inlineFragmentTypes === 'combine'

---
+++ packages/presets/near-operation-file/tests/fixtures/issue-406-child.ts
@@ -0,0 +1,11 @@
+export const UserEmailFragment = /* GraphQL */ `
+    fragment UserEmail on User {
+        email
+    }
+`;
+
+export const UserNameFragment = /* GraphQL */ `
+    fragment UserName on User {
+        name
+    }
+`;
