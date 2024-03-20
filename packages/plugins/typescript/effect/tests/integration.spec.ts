b2ed063518bc249c823891ef7c4864ee6b5c66d9
Attila Večerek <attila.vecerek@gmail.com>
2024-03-20 14:34:40 +0100

    feat(typescript-effect): export a basic GraphQLClient layer from within the generated SDK
    Co-authored-by: Tim Smart <hello@timsmart.co>

--- packages/plugins/typescript/effect/tests/integration.spec.ts
+++ packages/plugins/typescript/effect/tests/integration.spec.ts
@@ -8,28 +8,36 @@ import * as TypeScriptOperationsPlugin from '@graphql-codegen/typescript-operati
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import * as EffectPlugin from '../src/index.js';
 
+const sdkFileName = 'effect-sdk.ts';
+const sdkFilePath = path.join(__dirname, './test-files', sdkFileName);
+const typeDefs = parse(/* GraphQL */ `
+  type Query {
+    add(x: Int!, y: Int!): Int!
+  }
+`);
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers: {
+    Query: {
+      add: (_, { x, y }) => x + y,
+    },
+  },
+});
+const exampleQuery = /* GraphQL */ `
+  query Add($x: Int!, $y: Int!) {
+    add(x: $x, y: $y)
+  }
+`;
+const mockServer = mockGraphQLServer({
+  schema,
+  host: 'http://localhost:4000',
+  path: '/graphql',
+});
+
 describe('Effect Integration', () => {
+  afterAll(() => mockServer.done());
+
   it('should send requests correctly', async () => {
-    const sdkFileName = 'effect-sdk.ts';
-    const sdkFilePath = path.join(__dirname, './test-files', sdkFileName);
-    const typeDefs = parse(/* GraphQL */ `
-      type Query {
-        add(x: Int!, y: Int!): Int!
-      }
-    `);
-    const schema = makeExecutableSchema({
-      typeDefs,
-      resolvers: {
-        Query: {
-          add: (_, { x, y }) => x + y,
-        },
-      },
-    });
-    const exampleQuery = /* GraphQL */ `
-      query Add($x: Int!, $y: Int!) {
-        add(x: $x, y: $y)
-      }
-    `;
     const sdkCodeString = await codegen({
       schema: typeDefs,
       schemaAst: schema,
@@ -53,21 +61,16 @@ describe('Effect Integration', () => {
           'typescript-operations': {},
         },
         {
-          effect: {},
+          effect: { mode: 'mixed' },
         },
       ],
       config: {},
     });
     await fs.writeFile(sdkFilePath, sdkCodeString, 'utf-8');
-    const mockServer = mockGraphQLServer({
-      schema,
-      host: 'http://localhost:4000',
-      path: '/graphql',
-    });
-    const { runExampleQuery } = require('./test-files/run-example-query');
-    const { body } = await runExampleQuery(2, 3);
+    const { exampleQueries } = require('./test-files/run-example-query');
+    const { add } = exampleQueries('http://localhost:4000/graphql');
+    const { body } = await add(2, 3);
     expect(body.data.add).toBe(5);
-    mockServer.done();
     await fs.remove(sdkFilePath);
   });
 });
