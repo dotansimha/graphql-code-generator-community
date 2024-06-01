4f11ff1991e567b3856d115467f353986d2c7987
Attila Večerek <attila.vecerek@gmail.com>
2024-06-01 13:55:08 +0200

    chore: address issues uncovered by `@yarnpkg/doctor`
    - undeclared dependencies
    - strings referencing the node_modules directory
    
    For more details: https://yarnpkg.com/migration/pnp\#calling-the-doctor

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,5 @@ module.exports =
     : {
         rootDir: __dirname,
         projects: ['<rootDir>/packages/**/*/jest.config.js'],
-        resolver: './node_modules/bob-the-bundler/jest-resolver.cjs',
+        resolver: require.resolve('bob-the-bundler/jest-resolver'),
       };
