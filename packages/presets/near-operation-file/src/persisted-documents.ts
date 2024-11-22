85445d882c5e641d68932d566af7b852b966f6a6
Shane32 <shane@acdmail.com>
2024-11-22 16:26:40 -0500

    Add persisted operations to near-operation-file

---
+++ packages/presets/near-operation-file/src/persisted-documents.ts
@@ -0,0 +1,44 @@
+import { printExecutableGraphQLDocument } from '@graphql-tools/documents';
+import * as crypto from 'crypto';
+import { Kind, visit, type DocumentNode } from 'graphql';
+
+/**
+ * This function generates a hash from a document node.
+ */
+export function generateDocumentHash(
+  operation: string,
+  algorithm: 'sha1' | 'sha256' | (string & {}) | ((operation: string) => string)
+): string {
+  if (typeof algorithm === 'function') {
+    return algorithm(operation);
+  }
+  const shasum = crypto.createHash(algorithm);
+  shasum.update(operation);
+  return shasum.digest('hex');
+}
+
+/**
+ * Normalizes and prints a document node.
+ */
+export function normalizeAndPrintDocumentNode(documentNode: DocumentNode): string {
+  /**
+   * This removes all client specific directives/fields from the document
+   * that the server does not know about.
+   * In a future version this should be more configurable.
+   * If you look at this and want to customize it.
+   * Send a PR :)
+   */
+  const sanitizedDocument = visit(documentNode, {
+    [Kind.FIELD](field) {
+      if (field.directives?.some(directive => directive.name.value === 'client')) {
+        return null;
+      }
+    },
+    [Kind.DIRECTIVE](directive) {
+      if (directive.name.value === 'connection') {
+        return null;
+      }
+    },
+  });
+  return printExecutableGraphQLDocument(sanitizedDocument);
+}
