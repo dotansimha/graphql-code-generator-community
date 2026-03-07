bec593ee9ffcbde20adcf0854ee3be9317d0f5ec
Eddy Nguyen <github@eddeee888.me>
2026-03-08 00:16:26 +1100

    Add v15 vs v16 tests for nhost

--- packages/plugins/typescript/nhost/vitest.config.ts
+++ packages/plugins/typescript/nhost/vitest.config.ts
@@ -1,12 +1,17 @@
 import { defineProject, mergeConfig } from 'vitest/config';
 import { sharedConfig } from '../../../../vitest.config.js';
 
+let include = ['**/*.v16+.spec.ts'];
+if (process.env.GRAPHQL_VERSION === '15') {
+  include = ['**/*.v15.spec.ts'];
+}
+
 export default mergeConfig(
   sharedConfig,
   defineProject({
     test: {
       name: 'typescript-nhost',
-      include: ['**/*.spec.ts'],
+      include,
     },
   }),
 );
