8b7d9c3d55b62f61d82d191f2ace7f9d8c700a60
Eddy Nguyen <ch@eddeee888.me>
2026-06-10 23:57:35 +1000

    Update dev tests

--- packages/plugins/typescript/rtk-query/src/visitor.ts
+++ packages/plugins/typescript/rtk-query/src/visitor.ts
@@ -131,7 +131,7 @@ export { injectedRtkApi as api };
     const operationTypeString = operationType.toLowerCase();
 
     const functionsString =
-      `query: (variables) => ({ document: ${documentVariableName} unknown as string, variables })
+      `query: (variables) => ({ document: ${documentVariableName} as unknown as string, variables })
       ${this.injectTransformResponse(Generics)}`.trim();
 
     const endpointString = `
