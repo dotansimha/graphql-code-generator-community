9c635f97e0630d0e802bb4abb45b7beded8c0c0b
Eddy Nguyen <ch@eddeee888.me>
2026-06-23 22:44:31 +1000

    Set up test

---
+++ packages/plugins/typescript/rtk-query/tests/rtk-query.fragment.spec.ts
@@ -0,0 +1,54 @@
+import { buildSchema, parse } from 'graphql';
+import { plugin } from '../src/index.js';
+
+describe('RTK Query - fragments', () => {
+  it('generates TypedDocumentString for fragments correctly', async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      type Query {
+        say: Something
+      }
+
+      type Something {
+        id: ID!
+      }
+    `);
+
+    const document = parse(/* GraphQL */ `
+      fragment SomethingFrag on Something {
+        id
+      }
+    `);
+
+    const result = await plugin(schema, [{ document }], { importBaseApiFrom: './api' });
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "content": "export class TypedDocumentString<TResult, TVariables>
+        extends String
+        implements DocumentTypeDecoration<TResult, TVariables>
+      {
+        __apiType?: NonNullable<DocumentTypeDecoration<TResult, TVariables>['__apiType']>;
+        private value: string;
+        public __meta__?: Record<string, any> | undefined;
+
+        constructor(value: string, __meta__?: Record<string, any> | undefined) {
+          super(value);
+          this.value = value;
+          this.__meta__ = __meta__;
+        }
+
+        override toString(): string & DocumentTypeDecoration<TResult, TVariables> {
+          return this.value;
+        }
+      }
+      export const SomethingFragFragmentDoc = new TypedDocumentString(\`
+          fragment SomethingFrag on Something {
+        id
+      }
+          \`, {"fragmentName":"SomethingFrag"});
+      ",
+        "prepend": [],
+      }
+    `);
+  });
+});
